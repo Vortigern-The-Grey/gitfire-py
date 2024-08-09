4558319401c3c4b7b3aaf2a0d04a71707aae0e29
Vortigern-The-Grey <matthewkenyon2008@outlook.com>
2024-08-10 08:33:17 +1200

    added functionality to push local commits

--- git-fire.py
+++ git-fire.py
@@ -1,14 +1,31 @@
-
 import subprocess
 import os
 from datetime import date
 
 
-current_branch = subprocess.run("git symbolic-ref HEAD", capture_output=True).stdout.decode('utf-8').replace("\n", "").replace("\r", "")
-repo_base = subprocess.run("git rev-parse --show-toplevel", capture_output=True).stdout.decode('utf-8').replace("\n", "").replace("\r", "")
-user_email = subprocess.run("git config user.email", capture_output=True).stdout.decode('utf-8').replace("\n", "").replace("\r", "")
+current_branch = (
+    subprocess.run("git symbolic-ref HEAD", capture_output=True)
+    .stdout.decode("utf-8")
+    .replace("\n", "")
+    .replace("\r", "")
+)
+repo_base = (
+    subprocess.run("git rev-parse --show-toplevel", capture_output=True)
+    .stdout.decode("utf-8")
+    .replace("\n", "")
+    .replace("\r", "")
+)
+user_email = (
+    subprocess.run("git config user.email", capture_output=True)
+    .stdout.decode("utf-8")
+    .replace("\n", "")
+    .replace("\r", "")
+)
 current_epoch = date.today()
 new_branch = str(f"FIRE_({current_branch})_({user_email})_({current_epoch})")
+
+# push committed local changes
+os.system("git push -a")
 
 initial_branch = current_branch
 
@@ -24,8 +41,7 @@ if repo_base != os.getcwd():
 os.system("git add -A")
 message = str(f"Fire! Branch {current_branch}")
 
-os.system(f"git commit -m \"{message}\"")
+os.system(f'git commit -m "{message}"')
 
 os.system(f"git push -u origin {new_branch}")
 print("Push successful! Now git out!")
-
